61da250d2657e07ef4de2cc40f30e86b2bb51807
soberjan1 <aliguseynov48@gmail.com>
2023-04-26 17:21:03 +0300

    9 task

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,14 @@
+const btnEl = document.querySelector(".change-color");
+const spnEl = document.querySelector(".color");
+const bodyEl = document.querySelector(".widget");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+btnEl.addEventListener("click", () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  spnEl.textContent = getRandomHexColor();
+});
