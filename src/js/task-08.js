0307d9b4561bef79e8ddd426b57109464e8bce98
soberjan1 <aliguseynov48@gmail.com>
2023-04-26 11:40:35 +0300

    8 task

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,21 @@
+const submitEl = document.querySelector(".login-form");
 
+submitEl.addEventListener("submit", onLoginVal);
+
+function onLoginVal(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Все поля должны быть заполнены!");
+  } else {
+    const fullForm = {};
+
+    fullForm.email = email.value;
+    fullForm.password = password.value;
+    console.log(fullForm);
+    event.currentTarget.reset();
+  }
+}
